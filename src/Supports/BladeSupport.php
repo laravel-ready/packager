0395ca3989e35e50c82b4993201305b31868020c
Egoist <epsilonelittr@gmail.com>
2023-03-12 21:06:52 +0300

    Add custom blade renderer

---
+++ src/Supports/BladeSupport.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace LaravelReady\Packager\Supports;
+
+use Illuminate\View\Factory;
+use Illuminate\Events\Dispatcher;
+use Illuminate\View\FileViewFinder;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\View\Engines\PhpEngine;
+use Illuminate\View\Engines\CompilerEngine;
+use Illuminate\View\Engines\EngineResolver;
+use Illuminate\View\Compilers\BladeCompiler;
+use LaravelReady\Packager\Exceptions\FileNotFoundException;
+
+class BladeSupport
+{
+    private Filesystem $file;
+
+    public function __construct()
+    {
+        $this->file = new Filesystem();
+    }
+
+    /**
+     * Render template
+     *
+     * @param string $templatePath
+     * @param array $data
+     *
+     * @return string|null
+     * @throws \LaravelReady\Packager\Exceptions\FileNotFoundException
+     */
+    public function renderTemplate(string $templatePath, array $data = []): string
+    {
+        if (!$this->file->exists($templatePath)) {
+            throw new FileNotFoundException("Template file not found: {$templatePath}");
+        }
+
+        $engineResolver = new EngineResolver();
+        $bladeCompiler = new BladeCompiler(new Filesystem(), sys_get_temp_dir());
+        $engineResolver->register('blade', function () use ($bladeCompiler) {
+            return new CompilerEngine($bladeCompiler);
+        });
+
+        $engineResolver->register('php', function () {
+            return new PhpEngine();
+        });
+
+        $eventDispatcher = new Dispatcher();
+
+        $viewFactory = new Factory(
+            $engineResolver,
+            new FileViewFinder(
+                new Filesystem(),
+                ['resources/views']
+            ),
+            $eventDispatcher
+        );
+
+        $view = $viewFactory->file($templatePath, $data);
+
+        return $view->render();
+    }
+}
