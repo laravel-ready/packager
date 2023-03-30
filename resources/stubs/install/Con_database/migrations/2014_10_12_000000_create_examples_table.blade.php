df7ec5b44d81b894c43725477576d6aa99851d4d
Egoist <epsilonelittr@gmail.com>
2023-03-30 22:08:01 +0300

    Use package name as a example table name

--- resources/stubs/install/Con_database/migrations/2014_10_12_000000_create_examples_table.blade.php
+++ resources/stubs/install/Con_database/migrations/2014_10_12_000000_create_examples_table.blade.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('examples', function (Blueprint $table) {
+        Schema::create('{{ $PACKAGE_SLUG }}', function (Blueprint $table) {
             $table->id();
 
             // ...
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('examples');
+        Schema::dropIfExists('{{ $PACKAGE_SLUG }}');
     }
 };
